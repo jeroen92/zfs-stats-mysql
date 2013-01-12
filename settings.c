c1d229838a6ea761acb3a794bd5c6737f0bd6d8a
jeroen92 <jeroen@jeroen92.nl>
2013-01-13 00:08:16 +0100

    Stable release

---
+++ settings.c
@@ -0,0 +1,122 @@
+#include "main.h"
+
+// Initializing variables declared as extern in main.h
+CONFIGPAIR dbconfig[4] = {
+		{"dbhost"}, {"dbuser"}, {"dbpasswd"}, {"dbschema"}};
+CONFIGDATA configData = {&dbconfig[0], &dbconfig[1], &dbconfig[2], &dbconfig[3]};
+DATA COLLECTION[COLUMNS] = {
+		{"l2_io_error", 0},
+		{"l2_cksum_bad", 0},
+		{"l2_size", 0},
+		{"l2_hdr_size", 0},
+		{"l2_hits", 0},
+		{"l2_misses", 0},
+		{"l2_write_buffer_bytes_scanned", 0}
+};
+
+/*
+ * 	Checks if saved settings exists. If not, create a
+ * 	settings.xml file and prompts user to enter the
+ * 	correct settings, which will be saved in the settings.xml
+ *
+ * 	If file exists, parse the file and load settings.
+ */
+int initializeSettings() {
+	FILE* settingsFile;
+	// Try to open config file. If exists, load configuration
+	if (settingsFile = fopen(SETTINGSFILENAME, "r")) {
+		// Settings.xml exists. parsing...
+		settingsFile = fopen(SETTINGSFILENAME, "r");
+		parseSettingsFile(settingsFile);
+	} else {
+		// No settings.xml available. create one
+		printf("No config file found, creating one...\n");
+		if ((settingsFile = fopen(SETTINGSFILENAME, "a")) != NULL) {
+			promptUser();
+			createSettingsFile(SETTINGSFILENAME);
+		} else {
+			// Creating settings.xml failed
+			printf("Failed to create settings file\n");
+			return 1;
+		}
+	}
+
+	// Testing the filled in values
+	if(!initiateMySQL()) {
+		printf("Database entries are correct.\n");
+		printf("Verifying existence of tables...\n");
+		if (checkIfTableExists("l2arc_stats") == 0) {
+			printf("ZFS Stats tables are present.\n");
+		} else {
+			printf("Tables are not yet present, creating them...\n");
+				if (createDatabaseTables() == 0) {
+					printf("Table creation succeeded :)\n");
+				} else return 1;
+		}
+		return 0;
+	} else {
+		printf("\n\n Entered values are not correct.\nCould not connect to the specified DB\n");
+		return 1;
+	}
+}
+
+/*
+ * 	Prompts user to enter the database's:
+ * 	host, user, password and name.
+ * 	Puts the received text in configData.dbhost.
+ */
+void promptUser() {
+	printf("Database host:\t");
+	fgets(configData.dbhost->value, sizeof configData.dbhost->value, stdin);
+	trimWhiteSpace(configData.dbhost->value);
+
+	printf("\nDatabase user:\t");
+	fgets(configData.dbuser->value, sizeof configData.dbuser->value, stdin);
+	trimWhiteSpace(configData.dbuser->value);
+
+	printf("\nUsers password:\t");
+	fgets(configData.dbpasswd->value, sizeof configData.dbpasswd->value, stdin);
+	trimWhiteSpace(configData.dbpasswd->value);
+
+	printf("\nSchema name:\t");
+	fgets(configData.dbschema->value, sizeof configData.dbschema->value, stdin);
+	trimWhiteSpace(configData.dbschema->value);
+	return;
+}
+
+/*
+ * Fill the settings.xml file, given as the FILE* param. Iterates
+ * trough the dbconfig array. Every iteration is a new XML <key> and
+ * value, and so they will be written to the settings.xml.
+ * All the written XML nodes are part of the master node <config>
+ */
+int createSettingsFile(const char settingsFilename[]) {
+	int i;
+	FILE* configFile = fopen(settingsFilename, "a");
+	//printf("%s", dbconfig[1].value);
+	fwrite("<config>\n", sizeof(char), strlen("<config>\n"), configFile);
+	for (i = 0; i < (sizeof(dbconfig)/sizeof(CONFIGPAIR)); i++) {
+		fwrite("\t<", sizeof(char), strlen("\t<"), configFile);
+		fwrite(dbconfig[i].key, sizeof(char), strlen(dbconfig[i].key), configFile);
+		fwrite(">\n\t\t", sizeof(char), strlen(">\n\t\t"), configFile);
+		fwrite(dbconfig[i].value, sizeof(char), strlen(dbconfig[i].value), configFile);
+		fwrite("\n\t</", sizeof(char), strlen("\n\t</"), configFile);
+		fwrite(dbconfig[i].key, sizeof(char), strlen(dbconfig[i].key), configFile);
+		fwrite(">\n", sizeof(char), strlen(">\n"), configFile);
+	}
+	fwrite("</config>\n", sizeof(char), strlen("</config>\n"), configFile);
+	fclose(configFile);
+	return 0;
+}
+
+/*
+ *	Create the required tables in the specified database.
+ *	Return 0 on success and 1 on failure.
+ */
+int createDatabaseTables() {
+	char* queryString = "CREATE TABLE `l2arc_stats` (`date` datetime NOT NULL,`l2_hits` double DEFAULT NULL,`l2_misses` double DEFAULT NULL,`l2_cksum_bad` double DEFAULT NULL,`l2_io_error` double DEFAULT NULL,`l2_size` double DEFAULT NULL,`l2_hdr_size` double DEFAULT NULL,`l2_write_buffer_bytes_scanned` double DEFAULT NULL,PRIMARY KEY (`date`)) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+	if (executeQuery(queryString) == 0) {
+		return 0;
+	}
+	return 1;
+}
