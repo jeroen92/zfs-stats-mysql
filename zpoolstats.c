3cbb9f7c2daf2eb00793130dc4ce9eb41c6a462a
Super-User <root@develtestoi.(none)>
2013-03-17 21:59:57 +0100

    Added zpool io stats This version is unstable

---
+++ zpoolstats.c
@@ -0,0 +1,31 @@
+#include "zpoolstats.h"
+
+int collectZpoolStats() {
+	char* poolname = "rpool";
+	libzfs_handle_t *g_zfs = libzfs_init();
+
+	zpool_handle_t *zhp;
+	zhp = zpool_open_canfail(g_zfs, poolname);
+	
+	nvlist_t *configuration, *vdevroot;
+	vdev_stat_t *vdevstats;
+	iostatcollection statisticscollection;
+	configuration = zpool_get_config(zhp, NULL);
+
+	// Release the handle to the zpool
+	free(zhp);
+
+	// Put the vdev tree belonging to the pool in newconfig in newnvroot.
+	verify(nvlist_lookup_nvlist(configuration, ZPOOL_CONFIG_VDEV_TREE, &vdevroot) == 0);
+
+	int nelem;
+	// Load the new vdev stats in newvdevstat
+	verify(nvlist_lookup_uint64_array(vdevroot, ZPOOL_CONFIG_VDEV_STATS, (uint64_t **)&vdevstats, &nelem) == 0);
+
+	statisticscollection.readops = vdevstats->vs_ops[ZIO_TYPE_READ];
+	statisticscollection.writeops = vdevstats->vs_ops[ZIO_TYPE_WRITE];
+	statisticscollection.readbytes = vdevstats->vs_bytes[ZIO_TYPE_READ];
+	statisticscollection.writebytes = vdevstats->vs_bytes[ZIO_TYPE_WRITE];
+	
+	return 0;
+}
---
+++ zpoolstats.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <sys/fs/zfs.h>
+#include <libzfs.h>
+
+typedef struct iostatcollection {
+	uint64_t	readops;
+	uint64_t	writeops;
+	uint64_t	readbytes;
+	uint64_t	writebytes;
+} iostatcollection;
+
+int collectZpoolStats();
